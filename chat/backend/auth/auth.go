e37058f8a2d646d9a96f48c0951fa6580fea645d
vladof <f.vladislav@shinhanfinance.kz>
2023-11-29 17:50:59 +0600

    welcome

--- chat/backend/auth/auth.go
+++ chat/backend/auth/auth.go
@@ -5,6 +5,7 @@ import (
     "encoding/json"
 	"net/http"
 	"time"
+	"fmt"
 )
 
 
@@ -66,6 +67,39 @@ func Signin(w http.ResponseWriter, r *http.Request) {
 		Value: tokenString,
 		Expires: expirationTime,
 	})
+}
+
+func Welcome(w http.ResponseWriter, r *http.Request) {
+	c, err := r.Cookie("token")
+	if err != nil {
+		if err == http.ErrNoCookie {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	tknStr := c.Value
+	claims := &Claims{}
+
+	tkn, err := jwt.ParseWithClaims(tknStr, claims, func(token *jwt.Token) (any, error) {
+		return jwtKey, nil
+	} )
+	if err != nil {
+		if err == jwt.ErrSignatureInvalid {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	if !tkn.Valid {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	w.Write([]byte(fmt.Sprintf("Welcome %s!", claims.Username)))
 }
 
 
