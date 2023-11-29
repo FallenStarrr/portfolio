94a71d984f73f13344e1210cbb44891ffad65cdf
vladof <f.vladislav@shinhanfinance.kz>
2023-11-29 18:00:12 +0600

    refresh

--- chat/backend/auth/auth.go
+++ chat/backend/auth/auth.go
@@ -102,6 +102,57 @@ func Welcome(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(fmt.Sprintf("Welcome %s!", claims.Username)))
 }
 
+func Refresh(w http.ResponseWriter, r *http.Request) {
+	c, err := r.Cookie("token")
+	if err != nil {
+		if err == http.ErrNoCookie {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	tknStr := c.Value
+	claims := &Claims{}
+	tkn, err := jwt.ParseWithClaims(tknStr, claims, func(token *jwt.Token) (any, error) {
+		return jwtKey, nil
+	})
+	if err != nil {
+		if err == jwt.ErrSignatureInvalid {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	if !tkn.Valid {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+   if time.Until(claims.ExpiresAt.Time) > 30 * time.Second {
+	w.WriteHeader(http.StatusBadRequest)
+	return
+   }
+
+   expirationTime := time.Now().Add(5 * time.Second)
+   claims.ExpiresAt = jwt.NewNumericDate(expirationTime)
+   token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+   tokenString, err := token.SignedString(jwtKey)
+   if err != nil {
+	w.WriteHeader(http.StatusInternalServerError)
+	return
+   }
+
+   http.SetCookie(w, &http.Cookie{
+	Name: "token",
+	Value: tokenString,
+	Expires: expirationTime,
+   })
+
+
+}
+
 
 
 
